878d48861c3e3f6a8bdc513186e499553696668a
Ratmir Gaitov <gaitovrat@gmail.com>
2025-02-02 14:35:50 +0100

    Add comments

--- src/affinity_propagation.h
+++ src/affinity_propagation.h
@@ -8,18 +8,54 @@
 
 class AffinityPropagation {
   public:
+    /**
+     * @brief Construct a new Affinity Propagation object
+     *
+     * @param data
+     */
     AffinityPropagation(const Matrix &data);
 
+    /**
+     * @brief Construct a new Affinity Propagation object
+     *
+     */
     AffinityPropagation();
 
+    /**
+     * @brief Destroy the Affinity Propagation object
+     *
+     */
     virtual ~AffinityPropagation() = default;
 
+    /**
+     * @brief Calculate R and A matrixes
+     *
+     */
     void train();
+
+    /**
+     * @brief Get labels from R and A matrixes
+     *
+     * @return std::vector<uint64_t>
+     */
     std::vector<uint64_t> predict();
 
   private:
+    /**
+     * @brief Similarity between a and b
+     *
+     * @param a vector
+     * @param b vector
+     * @return double
+     */
     static double similarity(const std::vector<uint64_t> &a, const std::vector<uint64_t> &b);
 
+    /**
+     * @brief Median
+     *
+     * @param v vector
+     * @return double value
+     */
     static double median(std::vector<double> &v);
 
     FMatrix S;
